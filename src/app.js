605f26e3fdf767e1e7ced8333d8d3168daf73293
ineserdoura <ineserdoura@users.noreply.github.com>
2020-06-08 23:51:12 +0100

    Showing the current date

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-// Display Elements
+// Display elements
 
 function displayTemperature(response) {
   console.log(response.data);
@@ -26,3 +26,52 @@ let apiKey = "9261c308257e6cb61b3c077acec2b0f7";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Porto&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+// Display current date
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let date = now.getDate();
+let getOrdinal = function (n) {
+  let s = ["th", "st", "nd", "rd"],
+    v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
+let year = now.getFullYear();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentDate = document.querySelector("#current-date");
+currentDate.innerHTML = `${day} <br/>${month}, ${getOrdinal(date)} ${year}`;
+
+let lastUpdate = document.querySelector("#last-update");
+lastUpdate.innerHTML = `${hours}:${minutes}`;
