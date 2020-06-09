a6135351fdd9beb10f44d6be075286daffbe8d27
ineserdoura <ineserdoura@users.noreply.github.com>
2020-06-09 11:07:39 +0100

    Updated date function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,50 @@
+// Display current date
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let date = now.getDate();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let getOrdinal = function (n) {
+    let s = ["th", "st", "nd", "rd"],
+      v = n % 100;
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+  };
+  let year = now.getFullYear();
+  return `${day} <br/>${month}, ${getOrdinal(
+    date
+  )} ${year} <br/> <small>Last updated ${hours}:${minutes}</small>`;
+}
+
 // Display elements
 
 function displayTemperature(response) {
@@ -11,6 +58,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let cloudsElement = document.querySelector("#clouds");
+  let dateElement = document.querySelector("#current-date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
@@ -20,58 +68,10 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   cloudsElement.innerHTML = Math.round(response.data.clouds.all);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "9261c308257e6cb61b3c077acec2b0f7";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Porto&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
-
-// Display current date
-
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-let date = now.getDate();
-let getOrdinal = function (n) {
-  let s = ["th", "st", "nd", "rd"],
-    v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
-};
-let year = now.getFullYear();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let currentDate = document.querySelector("#current-date");
-currentDate.innerHTML = `${day} <br/>${month}, ${getOrdinal(date)} ${year}`;
-
-let lastUpdate = document.querySelector("#last-update");
-lastUpdate.innerHTML = `${hours}:${minutes}`;
